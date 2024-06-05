63ddfcfc616330fb9b0c23ec1575cbc0a7a80314
harold <155407111+DevHarold0-0@users.noreply.github.com>
2024-06-05 15:27:50 -0400

    Update grid and implement hover effect

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
 const container = document.querySelector("#container");
 
-const column = document.createElement("div");
-column.setAttribute("id", "column");
-container.appendChild(column);
-
-
-const row = document.createElement("div");
-row.setAttribute("id", "row");
-container.appendChild(row);
-
-
-
+// Grid
 for (let i = 0; i < 16; i++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
+    const row = document.createElement("div");
+    row.setAttribute("id", "row");
+    container.appendChild(row);
     
-    column.appendChild(box);
+    for (let k = 0; k < 16; k++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        row.appendChild(box);
+    }
 }
 
-for (let i = 0; i < 15; i++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    
-    row.appendChild(box);
-}
 
 
+// Hover effect
+const boxes = document.querySelectorAll(".box")
+let timeOut;
 
+boxes.forEach(div => {
+    div.addEventListener("mouseenter", (event) => {
+        event.target.style.backgroundColor = "orange";
+        
+        // timeOut = setInterval(() => {
+        //     event.target.style.backgroundColor = "black";
+        // }, 800)
+
+        // console.log("you are in");
+    })
+})
